a83c97dfb99c3e814da076b0e67a2cd276451b7d
Menno de Ridder <menno@sebsoft.nl>
2013-12-19 10:21:04 +0100

    Fixed: install.xml, for_user_email must be char not int..

--- blocks/voucher/version.php
+++ blocks/voucher/version.php
@@ -12,6 +12,6 @@
  */
 
 $plugin = new stdClass();
-$plugin->version  = 2013121101;
+$plugin->version  = 2013121901;
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->cron     = 1;
